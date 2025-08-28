13c48fe4718e9d351c39b6762e418ec0096a656b
Maxim Bakshaev <risen.2011@mail.ru>
2025-08-28 16:46:24 +0900

    Добавлен bearer_transport, создан UserIdType и access_token, миграция токена (#16)

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,20 +1,18 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users_db_sqlalchemy import (
-    SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase,
-)
-
-from .mixins.id_int_pk import IdIntPkMixin
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 from . import Base
+from core.types.user_id import UserIdType
+from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    """Пользователь с id"""
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    """Пользователь с id, почтой, хеш-паролем"""
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
