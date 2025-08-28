34f006a2c34e3afd136a457c4d891b9773aeca53
MaxBakshaev <risen.2011@mail.ru>
2025-08-28 15:41:23 +0900

    Изменена модель User

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,12 +1,21 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
 )
 
 from .mixins.id_int_pk import IdIntPkMixin
 
 from . import Base
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     """Пользователь с id"""
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
