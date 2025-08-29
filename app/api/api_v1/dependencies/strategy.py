306554716384255fdfb0f3942b04a5e6c682931d
MaxBakshaev <risen.2011@mail.ru>
2025-08-29 10:41:56 +0900

    Созданы функции получения БД users и access_tokens

--- app/api/api_v1/dependencies/strategy.py
+++ app/api/api_v1/dependencies/strategy.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from fastapi_users.authentication.strategy.db import DatabaseStrategy
 
 from core import settings
+from .access_tokens import get_access_tokens_db
 
 if TYPE_CHECKING:
     from core.models import AccessToken
