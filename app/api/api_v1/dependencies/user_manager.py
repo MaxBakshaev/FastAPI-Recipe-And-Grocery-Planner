350bdeb21e4e1d0ce2c8322b677f9e7e8d5f1a4c
MaxBakshaev <risen.2011@mail.ru>
2025-08-29 12:27:14 +0900

    Создана dependency для менеджера пользователей

--- app/api/api_v1/dependencies/user_manager.py
+++ app/api/api_v1/dependencies/user_manager.py
@@ -1,5 +1,18 @@
+from typing import Annotated, TYPE_CHECKING
+
+from fastapi import Depends
+
+from app.core.authentication.user_manager import UserManager
+from .users import get_users_db
+
+if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 
-
-async def get_user_manager(user_db=Depends(get_user_db)):
+async def get_user_manager(
+    user_db: Annotated[
+        "SQLAlchemyUserDatabase",
+        Depends(get_users_db),
+    ]
+):
     yield UserManager(user_db)
